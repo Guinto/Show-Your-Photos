6db07ab5e3b134dca7ab4797270f2f89afc52741
Trent Ellingsen <trentellingsen@gmail.com>
2012-08-30 16:23:13 -0700

    got an initial working model
    It puts a public set of photos on a page and each can be hovered over to
    flip and reveal another photo

--- js/flickr.js
+++ js/flickr.js
@@ -1,14 +1,54 @@
-function flickrStuff() {
-$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
-  {
-    tags: "mount rainier",
-    tagmode: "any",
-    format: "json"
-  },
-  function(data) {
-    $.each(data.items, function(i,item){
-      $("<img/>").attr("src", item.media.m).appendTo("#images");
-      if ( i == 3 ) return false;
-    });
-  });
+function getImgSrcFromResponse(response, index) {
+	var farmID = response.photos.photo[index].farm;
+	var serverID = response.photos.photo[index].server;
+	var photoID = response.photos.photo[index].id;
+	var secret = response.photos.photo[index].secret;
+	return "http://farm" + farmID + ".staticflickr.com/" + serverID + "/"
+			+ photoID + "_" + secret + "_n.jpg";
+}
+
+function createPanels(response) {
+	for (var i = 0; i < response.photos.photo.length - 1; i += 2) {
+		var fsrc = getImgSrcFromResponse(response, i);
+		var bsrc = getImgSrcFromResponse(response, i + 1);
+		createFlipPanel(fsrc, bsrc, i / 2);
+	}
+	setupFlip();
+}
+
+function createFlipPanel(fsrc, bsrc, id) {
+	makeHoverPanel(id);
+	makeFront(id);
+	makeBack(id);
+	insertImgs(fsrc, bsrc, id);
+}
+
+function insertImgs(fsrc, bsrc, id) {
+	$("<img/>", {
+		src: fsrc
+	}).appendTo("#" + id + "front");
+	$("<img/>", {
+		src: bsrc
+	}).appendTo("#" + id + "back");
+}
+
+function makeBack(id) {
+	$("<div/>", {
+		"class": "back",
+		"id": id + "back"
+	}).appendTo("#" + id);
+}
+
+function makeFront(id) {
+	$("<div/>", {
+		"class": "front",
+		"id": id + "front"
+	}).appendTo("#" + id);
+}
+
+function makeHoverPanel(id) {
+	$("<div/>", {
+		"class": "hover panel",
+		"id": id
+	}).appendTo(".container");
 }
